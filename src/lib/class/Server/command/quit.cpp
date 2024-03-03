3bb172298f1fcaddb4ef308e62ec8e64a0ce269f
Saphirac <mcourtoi@student.42.fr>
2024-03-03 23:11:55 +0100

    QUIT cmd [OK]

--- src/lib/class/Server/command/quit.cpp
+++ src/lib/class/Server/command/quit.cpp
@@ -3,11 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:25:39 by jodufour          #+#    #+#             */
-/*   Updated: 2024/02/08 01:00:16 by jodufour         ###   ########.fr       */
+/*   Updated: 2024/03/03 23:11:41 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // TODO
+
+void Server::quit(Client &sender, std::vector<std::string> const &params)
+{
+	if (params[0])
+		sender->disconnect(params[0]);
+	else
+		sender->disconnect("QUIT command received, you are being disconnected.");
+}
