3dd0e10a4c436513fad2577acaa9b8b620f238d3
Saphirac <mcourtoi@student.42.fr>
2024-03-05 20:32:31 +0100

    TOPIC command [OK]

--- src/lib/class/Server/command/invite.cpp
+++ src/lib/class/Server/command/invite.cpp
@@ -3,11 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   invite.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:27:28 by jodufour          #+#    #+#             */
-/*   Updated: 2024/02/08 00:59:54 by jodufour         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:28:14 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// TODO
+#include "class/Server.hpp"
+#include "ft_irc.hpp"
+#include "replies.hpp"
+
+/**
+ * @brief Invite a user to a channel, adding him to the Invite list
+ *
+ * @param sender the client inviting
+ * @param params the command parameters (nickname, channel)
+ */
+void Server::invite(Client &sender, std::vector<std::string> const &params)
+{
+	
+}
--- src/lib/class/Server/command/topic.cpp
+++ src/lib/class/Server/command/topic.cpp
@@ -6,7 +6,55 @@
 /*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 17:26:47 by jodufour          #+#    #+#             */
-/*   Updated: 2024/03/05 18:08:01 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:16:19 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "class/Server.hpp"
+#include "ft_irc.hpp"
+#include "replies.hpp"
+
+inline static void	__set_topic(Nickname &sender, Channel &channel, std::string const &topic)
+{
+	if (channel.has_mode(TopicProtected) && !channel.has_operator(sender))
+		sender.append_to_msg_out(format_reply(ERR_CHANOPRIVSNEEDED, sender));
+	else
+	{
+		channel.set_topic(topic);
+		channel.broadcast_to_all_members("TOPIC : " + topic);
+	}
+}
+
+inline static void __display_topic(Client &sender, Channel &channel)
+{
+	std::string const &topic = channel.get_topic();
+	char const *chan_name_str = channel.get_name().c_str();
+	if (topic.empty())
+		sender.append_to_msg_out(format_reply(RPL_NOTOPIC, chan_name_str));
+	else
+		sender.append_to_msg_out(format_reply(RPL_TOPIC, chan_name_str, topic));
+}
+
+void	Server::topic(Client &sender, std::vector<std::string> const &params)
+{
+	if (params.empty())
+	{
+		sender.append_to_msg_out(format_reply(ERR_NEEDMOREPARAMS, "TOPIC"));
+		return ;
+	}
+	
+	ChannelName const &chan_name = params[0];
+	Channel const &channel = this->_channels_by_name.find(chan_name);
+	Nickname const &nickname = sender.get_nickname();
+	
+	if (channel == this->_channels_by_name.end() || !channel.has_member(nickname))
+	{
+		sender.append_to_msg_out(format_reply(ERR_NOTONCHANNEL, chan_name.c_str()));
+		return ;
+	}
+	
+	if (!params[1].empty() && params[1][0] == ':')
+		__set_topic(nickname, channel, params[1].substr(1));
+	else
+		__display_topic(sender, channel);
+}
