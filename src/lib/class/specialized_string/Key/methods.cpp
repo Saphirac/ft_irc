02916131fdd097b38927da93d792202e17a41667
jodufour <jodufour@student.42.fr>
2024-03-07 22:16:59 +0100

    Fix the `Key::is_valid()` method [OK] Update the RFC summary                                  [DOING]   List the command steps with <ol> tags                 [DOING]     QUIT                                                [OK]     JOIN                                                [OK]     PART                                                [TODO]   List the possible assertion failures and how to react [DOING]     JOIN                                                [OK]     PART                                                [TODO]

--- src/lib/class/specialized_string/Key/methods.cpp
+++ src/lib/class/specialized_string/Key/methods.cpp
@@ -6,13 +6,16 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 22:51:47 by jodufour          #+#    #+#             */
-/*   Updated: 2024/03/05 15:40:00 by jodufour         ###   ########.fr       */
+/*   Updated: 2024/03/06 20:05:05 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "abnf_components.hpp"
 #include "class/specialized_string/Key.hpp"
 
+#define MINIMUM_LENGTH_FOR_KEY      1
+#define MAXIMUM_LENGTH_FOR_KEY      23
+#define EXCLUDED_CHARACTERS_FOR_KEY "\t\n\v\f\r ,"
 /**
  * @brief Checks whether the key is valid.
  *
@@ -20,5 +23,8 @@
  */
 bool Key::is_valid(void) const
 {
-	return lfcrspcl.find(this->at(0)) == std::string::npos && this->find_first_of(lfcrsp, 1) == std::string::npos;
+	size_t const len = this->size();
+
+	return len >= MINIMUM_LENGTH_FOR_KEY && len <= MAXIMUM_LENGTH_FOR_KEY
+	    && this->find_first_of(EXCLUDED_CHARACTERS_FOR_KEY) == std::string::npos;
 }
