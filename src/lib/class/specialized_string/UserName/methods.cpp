f8f4dca1bfc861bb516da5aefc9eeadc8cd471cb
Gurvan Le Minier <gleminier2@gmail.com>
2024-03-11 10:12:42 +0100

    change is_valid function to manage empty string and add all the nolfcrspat characters

--- src/lib/class/specialized_string/UserName/methods.cpp
+++ src/lib/class/specialized_string/UserName/methods.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   methods.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gle-mini <gle-mini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 22:42:26 by jodufour          #+#    #+#             */
-/*   Updated: 2024/03/04 22:42:32 by jodufour         ###   ########.fr       */
+/*   Updated: 2024/03/11 10:12:00 by gle-mini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,16 @@
  *
  * @return `true` if the username is valid, `false` otherwise.
  */
-bool UserName::is_valid(void) const { return this->find('@') == std::string::npos; }
+bool UserName::is_valid(void) const
+{
+	if (this->empty())
+		return false;
+
+	const char forbiddenChars[] = {0x00, 0x0A, 0x0D, 0x20, 0x40};
+	for (size_t i = 0; i < 5; ++i)
+	{
+		if (this->find(forbiddenChars[i]) != std::string::npos)
+			return false;
+	}
+	return true;
+}
