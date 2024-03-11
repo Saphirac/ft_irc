310bc1946942dba571603cbb4bf8c7b3545dbbd8
Gurvan Le Minier <gleminier2@gmail.com>
2024-03-11 10:10:25 +0100

    accept dash in nickname

--- src/lib/class/specialized_string/NickName/methods.cpp
+++ src/lib/class/specialized_string/NickName/methods.cpp
@@ -6,7 +6,7 @@
 /*   By: gle-mini <gle-mini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 22:37:23 by jodufour          #+#    #+#             */
-/*   Updated: 2024/03/11 09:03:25 by gle-mini         ###   ########.fr       */
+/*   Updated: 2024/03/11 09:38:46 by gle-mini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ bool NickName::is_valid(void) const
 	if (this->empty())
 		return false;
 	return this->size() <= MAXIMUM_LENGTH_FOR_NICKNAME && (letter + special).find(this->at(0)) != this->npos
-	    && this->find_first_not_of(letter + digit + special, 1) == this->npos;
+	    && this->find_first_not_of(letter + digit + special + dash, 1) == this->npos;
 }
