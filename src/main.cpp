a40a06ce09fa5a918fbe0986cd14eef074eb52f7
Gurvan Le Minier <gleminier2@gmail.com>
2024-03-13 11:57:10 +0100

    add header comment and define in

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,28 @@
 /*   By: gle-mini <gle-mini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 19:31:04 by mcourtoi          #+#    #+#             */
-/*   Updated: 2024/03/12 05:42:02 by gle-mini         ###   ########.fr       */
+/*   Updated: 2024/03/13 11:55:56 by gle-mini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "class/Server.hpp"
 #include "class/specialized_string/Password.hpp"
+#include <cctype>
 #include <cstdlib>
 #include <iostream>
-#include <cctype>
 
+#define MINIMUM_PORT 1024
+#define MAXIMUM_PORT 49151
+
+/**
+ * @brief check if the main arguments are correct
+ *
+ * The server can only use ports with numbers 1024-49151 that are registered ports;
+ *
+ * @param ac number of arguments
+ * @param av arguments
+ * @return true if the arguments are correct
+ */
 inline static bool check_main_arguments(int const ac, char const *const *const av)
 {
 	if (ac != 3)
@@ -32,24 +44,24 @@ inline static bool check_main_arguments(int const ac, char const *const *const a
 		}
 	}
 	int const port = atoi(av[1]);
-	if (!(port >= 1024 && port <= 49151))
+	if (!(port >= MINIMUM_PORT && port <= MAXIMUM_PORT))
 	{
 		std::cerr << "incorrect: port must be between 1024 and 49151" << std::endl;
 		return false;
 	}
 
 	if (!Password(av[2]).is_valid())
-    {
-        std::cerr << "incorrect: password does not respect correct notation" << std::endl;
+	{
+		std::cerr << "incorrect: password does not respect correct notation" << std::endl;
 		return false;
-    }
-    
+	}
+
 	return true;
 }
 int main(int const ac, char const *const *const av)
 {
 	srand(time(NULL));
-	
+
 	if (!check_main_arguments(ac, av))
 		return 1;
 
